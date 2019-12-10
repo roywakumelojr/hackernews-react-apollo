72ae1350020a31a4bec3740f9670ebdf60abf6e2
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-12-10 15:07:51 -0500

    sign up and login mutation complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 import { AUTH_TOKEN } from '../constants'
 
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
+
 class Login extends Component {
   state = {
     login: true, // switch between Login and SignUp
@@ -37,16 +53,22 @@ class Login extends Component {
           />
         </div>
         <div className="flex mt3">
-          <div className="pointer mr2 button" onClick={() => this._confirm()}>
-            {login ? 'login' : 'create account'}
-          </div>
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+            variables={{ email, password, name }}
+            onCompleted={data => this._confirm(data)}
+          >
+            {mutation => (
+              <div className="pointer mr2 button" onClick={mutation}>
+                {login ? 'login' : 'create account'}
+              </div>
+            )}
+          </Mutation>
           <div
             className="pointer button"
             onClick={() => this.setState({ login: !login })}
           >
-            {login
-              ? 'need to create an account?'
-              : 'already have an account?'}
+            {login ? 'need to create an account?' : 'already have an account?'}
           </div>
         </div>
       </div>
