99747ac613471dd770e72867ac04880586b252a3
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-12-10 20:55:12 -0500

    Realtime Updates with GraphQL Subscriptions Complete

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-import * as serviceWorker from './serviceWorker';
-import { setContext } from 'apollo-link-context'
-import { AUTH_TOKEN } from './constants'
+import * as serviceWorker from './serviceWorker'
 
-
-// 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
-// 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'http://localhost:4000',
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -25,26 +26,41 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
+      authorization: token ? `Bearer ${token}` : '',
+    },
   }
 })
 
-// 3
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  link: httpLink,
-  cache: new InMemoryCache()
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
 })
 
-// 4
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink),
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
+
 ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
-
-serviceWorker.unregister();
+serviceWorker.unregister()
